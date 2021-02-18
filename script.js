116f47a8837c79c23116ad66fc9b4730df8a17c6
Adelina <adelina.sahlean@gmail.com>
2021-02-19 00:26:11 +0100

    Harmonies not working yet

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 "use strict";
 
-window.addEventListener("DOMContentLoaded",init);
+window.addEventListener("DOMContentLoaded", init);
 
-let index = 3; 
+let index = 3;
 
 function init() {
   // set up the eventlistener
-  document.querySelector("#colorpicker").addEventListener("input", getColorInput);
+  document
+    .querySelector("#colorpicker")
+    .addEventListener("input", getColorInput);
 }
 
 function getColorInput(event) {
@@ -25,7 +27,7 @@ function selectColor(color) {
   const rgb = hex2RGB(color);
   const hsl = rgb2HSL(rgb);
   const baseColor = rgb2HSL(hex2RGB(color));
-//calculate Harmony
+  //calculate Harmony
   calculateHarmony(baseColor);
   displayColorInfo(color, index);
 }
@@ -35,7 +37,6 @@ function displayColorInfo(color, index) {
   // get the code for selected color
   const rgb = hex2RGB(color);
   const hsl = rgb2HSL(rgb);
-
   // display those different colors ...
   showHexColor(color, index);
   showRbgColor(rgb, index);
@@ -66,9 +67,9 @@ function hex2RGB(color, index) {
   r = parseInt(r, 16);
   g = parseInt(g, 16);
   b = parseInt(b, 16);
-//
-console.log(`RGB: ${r}, ${g}, ${b}`);
-//
+  //
+  console.log(`RGB: ${r}, ${g}, ${b}`);
+  //
   return { r, g, b };
 }
 
@@ -116,11 +117,59 @@ function rgb2HSL(rgbObject) {
   //
   console.log("hsl(%f,%f%,%f%)", h, s, l);
   //
-
   return { h, s, l };
 }
 
-//functions showing the code 
+// RGB TO CSS (NOT VERY IMPORTANT)
+// function rgb2CSS(rgbObject) {
+//   return `${​​rgb.r}​​, ${​​rgb.g}​​, ${​​rgb.b}​​`;
+// }
+
+//HSL TO RGB - MIGHT BE
+function hsl2RGB(hsl) {
+  const h = hsl.h;
+  const s = hsl.s / 100;
+  const l = hsl.l / 100;
+
+  let c = (1 - Math.abs(2 * l - 1)) * s,
+    x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
+    m = l - c / 2,
+    r = 0,
+    g = 0,
+    b = 0;
+  if (0 <= h && h < 60) {
+    r = c;
+    g = x;
+    b = 0;
+  } else if (60 <= h && h < 120) {
+    r = x;
+    g = c;
+    b = 0;
+  } else if (120 <= h && h < 180) {
+    r = 0;
+    g = c;
+    b = x;
+  } else if (180 <= h && h < 240) {
+    r = 0;
+    g = x;
+    b = c;
+  } else if (240 <= h && h < 300) {
+    r = x;
+    g = 0;
+    b = c;
+  } else if (300 <= h && h < 360) {
+    r = c;
+    g = 0;
+    b = x;
+  }
+  r = Math.round((r + m) * 255);
+  g = Math.round((g + m) * 255);
+  b = Math.round((b + m) * 255);
+
+  return { r, g, b };
+}
+
+//functions showing the code
 
 function showHexColor(hex, index) {
   document.querySelector(`#colorinfo${index} .hex .value`).textContent = hex;
@@ -145,5 +194,75 @@ function showColorBox(rgb, index) {
 }
 
 function calculateHarmony(baseColor) {
+  console.log("Time for fun");
+  const harmony = document.getElementsByName("harmony").value;
+  //onclick event
+  //document.querySelector("harmony").addEventListener("click", value);
 
+  if (harmony === "analogous") {
+    console.log("Harmony is Analogous");
+    let analog = showAnalogColors(baseColor);
+    console.log(analog);
+    //
+  } else if (harmony === "monochromatic") {
+    console.log("Harmony is Monochromatic");
+    let mono = showMonoColors(baseColor);
+    console.log(mono);
+    //
+  } else if (harmony === "triad") {
+    console.log("Harmony is triad");
+    let triad = showTriadColors(baseColor);
+    console.log(triad);
+    //
+  } else if (harmony === "complementary") {
+    console.log("Harmony is Complementary");
+    let complem = showCompletColors(baseColor);
+    console.log(complem);
+    //
+  } else if (harmony === "compound") {
+    console.log("Harmony is Compound");
+    let compound = showCompoundColors(baseColor);
+    console.log(compound);
+    //
+  } else if (harmony === "shades") {
+    console.log("Harmony shows shades");
+    let shades = showShadesColors(baseColor);
+    console.log(shades);
+    //
+  }
 }
+
+// displaying harmonies
+function showAnalogColors(baseColor) {
+  let ten = 10;
+  let newH = baseColor.h + ten;
+  let newS = baseColor.s;
+  let newL = baseColor.l;
+  //
+  const analog_a = { h: newH, s: newS, l: newL };
+  //
+  newH = newH + ten;
+  const analog_b = { h: newH, s: newS, l: newL };
+  //
+  newH = newH + ten;
+  const analog_d = { h: newH, s: newS, l: newL };
+  //
+  newH = newH + ten;
+  const analog_e = { h: newH, s: newS, l: newL };
+  //
+  hsl2RGB(analog_a, "a");
+  hsl2RGB(analog_b, "b");
+  hsl2RGB(hslObject, "c");
+  hsl2RGB(analog_d, "d");
+  hsl2RGB(analog_e, "e");
+}
+
+function showMonoColors(baseColor) {}
+
+function showTriadColors(baseColor) {}
+
+function showCompletColors(baseColor) {}
+
+function showCompoundColors(baseColor) {}
+
+function showShadesColors(baseColor) {}
